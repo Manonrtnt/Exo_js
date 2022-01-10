7e3ed81f3b082c6c69eb09b41bb331cf50e3c253
Manonrtnt <70939460+Manonrtnt@users.noreply.github.com>
2022-01-10 11:28:44 +0100

    Ajout code pour fenêtre modale

--- exo1/main.js
+++ exo1/main.js
@@ -28,3 +28,23 @@ function ajouter(){
         nbrCompte.innerHTML = "<p>Il y a "+cpt+" compte utilisateur </p>";
     }
 };
+
+
+// MODAL //
+/* Modal */
+//Variable qui récupére le modal
+let modal = document.getElementById("myModal");
+//Récupération du <span> pour fermer le modal
+let span = document.getElementsByClassName("close")[0];
+//fonction fermeture du modal quand l'utilisateur clic sur le <span> (x)
+span.onclick = function() {
+    modal.style.display = "none";
+}
+//fonction qui ouvre le modal
+function openModal(e){
+    modal.style.display = "block";
+    //récupération du paragraphe 
+    let btUpdate = document.getElementById("btUpdate");
+    //injection du bouton dans le paragraphe
+    btUpdate.innerHTML = "<input type='button' value='Modifier' onclick='updateUser("+e+")'></input>";
+}
