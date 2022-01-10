d4b9c69d4580277aade0feb046276a18675d7a98
Manonrtnt <70939460+Manonrtnt@users.noreply.github.com>
2022-01-10 11:58:21 +0100

    Ajout erreur si un des champs n'est pas rempli

--- exo1/main.js
+++ exo1/main.js
@@ -6,8 +6,10 @@ let cpt = 0;
 
 let resultat = document.getElementById("resultat");
 let nbrCompte = document.getElementById("nbrCompte");
+let error = document.getElementById("error");
 
-function ajouter(){
+/* Ajouter user */
+function add(){
     let name = document.getElementById("name").value
     let firstName = document.getElementById("firstName").value;
     let login = document.getElementById("login").value;
@@ -21,6 +23,8 @@ function ajouter(){
         resultat.innerHTML += "<p>Nom : "+name+", Prenom : "+ firstName+", Login : "+login+", Mot de passe : "+mdp+"</p>";
 
         cpt++;
+    } else {
+        error.innerHTML = "Veuillez remplir tous les champs avant d'ajouter un utilisateur";
     }
 
     // compteur de compte
@@ -29,22 +33,25 @@ function ajouter(){
     }
 };
 
+// /* Modal */
+// //Variable qui récupére le modal
+// let modal = document.getElementById("myModal");
+// //Récupération du <span> pour fermer le modal
+// let span = document.getElementsByClassName("close")[0];
+// //fonction fermeture du modal quand l'utilisateur clic sur le <span> (x)
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+// //fonction qui ouvre le modal
+// function openModal(e){
+//     modal.style.display = "block";
+//     //récupération du paragraphe 
+//     let btUpdate = document.getElementById("btUpdate");
+//     //injection du bouton dans le paragraphe
+//     btUpdate.innerHTML = "<input type='button' value='Modifier' onclick='updateUser("+e+")'></input>";
+// }
 
-// MODAL //
-/* Modal */
-//Variable qui récupére le modal
-let modal = document.getElementById("myModal");
-//Récupération du <span> pour fermer le modal
-let span = document.getElementsByClassName("close")[0];
-//fonction fermeture du modal quand l'utilisateur clic sur le <span> (x)
-span.onclick = function() {
-    modal.style.display = "none";
-}
-//fonction qui ouvre le modal
-function openModal(e){
-    modal.style.display = "block";
-    //récupération du paragraphe 
-    let btUpdate = document.getElementById("btUpdate");
-    //injection du bouton dans le paragraphe
-    btUpdate.innerHTML = "<input type='button' value='Modifier' onclick='updateUser("+e+")'></input>";
-}
+/* Supprimer user 
+function delete(){
+
+}*/
