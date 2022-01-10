721438d77c0dc34fcbe9c00f627729c0e931d719
Manonrtnt <70939460+Manonrtnt@users.noreply.github.com>
2022-01-10 11:19:45 +0100

    Création fonction ajouter()

--- exo1/main.js
+++ exo1/main.js
@@ -1,1 +1,30 @@
 console.log("test");
+
+// Variables 
+let tab = [];
+let cpt = 0;
+
+let resultat = document.getElementById("resultat");
+let nbrCompte = document.getElementById("nbrCompte");
+
+function ajouter(){
+    let name = document.getElementById("name").value
+    let firstName = document.getElementById("firstName").value;
+    let login = document.getElementById("login").value;
+    let mdp = document.getElementById("mdp").value;
+    console.log(name);
+
+    //test si champ vide
+    if(name != "" && firstName != "" && login != "" && mdp != ""){
+        tab.push([name, firstName, login, mdp]);
+
+        resultat.innerHTML += "<p>Nom : "+name+", Prenom : "+ firstName+", Login : "+login+", Mot de passe : "+mdp+"</p>";
+
+        cpt++;
+    }
+
+    // compteur de compte
+    if (cpt>0){
+        nbrCompte.innerHTML = "<p>Il y a "+cpt+" compte utilisateur </p>";
+    }
+};
